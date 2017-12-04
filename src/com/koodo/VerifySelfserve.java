92a2865e3247e131dcf0ae950718b2121ccb6169
darrendeng777 <darren.deng1@telus.com>
2017-12-04 09:16:50 -0500

    this is the initial push

---
+++ src/com/koodo/VerifySelfserve.java
@@ -0,0 +1,53 @@
+package com.koodo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class VerifySelfserve {
+	
+	//1.to create webdriver
+	WebDriver driver = new FirefoxDriver();
+	
+	//2.go to koodo site
+	@BeforeClass
+	public void goToKoodo() {
+		driver.get("https://www.koodomobile.com");
+	}
+	
+	//3.verify if you are in koodo site
+	@Test(priority=1)
+	public void verifyKoodoTitle() {
+		String expectTitle = "Choose Happy | Koodo Mobile";
+		String actualTitle = driver.getTitle();
+		System.out.println("Your page title is "+actualTitle);
+		Assert.assertEquals(actualTitle, expectTitle);
+		
+	}
+	
+
+	//4.click on self serve
+	@Test(priority=2)
+	public void verifyClickSelfServe() {
+		driver.findElement(By.xpath("(//a[contains(text(),'Self Serve')])[2]")).click();
+	}
+	
+	//5.type in credential
+	@Test(priority=3)
+	public void typeCredential() {
+		driver.findElement(By.xpath("//input[@name='IDToken1']")).sendKeys("sheldon_hua@telusinternal.com");
+		driver.findElement(By.xpath("//input[@name='IDToken2']")).sendKeys("Koodouat1234");
+		
+	}
+	
+	//6.close driver
+	@AfterClass
+	public void closeMyDriver() {
+		driver.close();
+	}
+
+}
